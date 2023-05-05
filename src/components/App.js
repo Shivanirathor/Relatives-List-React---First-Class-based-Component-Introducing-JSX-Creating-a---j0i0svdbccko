841d064e007d2f2ca5d4adb2458b2de8433c56f3
Shivanirathor <Shivanirathor034@gmail.com>
2023-05-06 00:34:23 +0530

    key

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,56 @@
-import React, { Component } from "react";
+// import React, { Component } from "react";
+// import '../styles/App.css';
+
+// class App extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             diwaliList: [
+//                 { id: 1, name: "Kim" },
+//                 { id: 2, name: "Tim" },
+//                 { id: 3, name: "Sam" },
+//                 { id: 4, name: "Hema" },
+//                 { id: 5, name: "Tim" },
+              
+//             ]
+//         };
+//     }
+
+//     render() {
+//         const relativeList = this.state.diwaliList.map((data) => {
+//             return <li key={`relativeListItem${data.id}`}>{data.name}</li>;
+//         });
+
+//         return (
+//             <div id="main">
+//                 <ol key="diwaliList">{relativeList}</ol>
+//             </div>
+//         );
+//     }
+// }
+
+// export default App;
+
+// ==============================================//
+
+import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            diwaliList: [
-                { id: 1, name: "Kim" },
-                { id: 2, name: "Tim" },
-                { id: 3, name: "Sam" },
-                { id: 4, name: "Hema" },
-                { id: 5, name: "Tim" },
-              
-            ]
-        };
-    }
+  render() {
+    const relatives = ["Amit", "Rita", "Rahul", "Kavya"];
 
-    render() {
-        const relativeList = this.state.diwaliList.map((data) => {
-            return <li key={`relativeListItem${data.id}`}>{data.name}</li>;
-        });
-
-        return (
-            <div id="main">
-                <ol key="diwaliList">{relativeList}</ol>
-            </div>
-        );
-    }
+    return (
+      <div>
+        <h2>Relatives to Visit</h2>
+        <ol key="relativeList">
+          {relatives.map((relative, index) => (
+            <li key={`relativeListItem${index + 1}`}>{relative}</li>
+          ))}
+        </ol>
+      </div>
+    );
+  }
 }
 
 export default App;
